1afb67b334c3f11985c5b1f9a9668bc5d01b694e
stamacake <30477203+stamacake@users.noreply.github.com>
2022-08-30 22:28:44 +0300

    226. Invert Binary Tree
    https://leetcode.com/submissions/detail/787427276/

---
+++ 226.java
@@ -0,0 +1,39 @@
+/* Given the root of a binary tree, invert the tree, and return its root.
+Input: root = [4,2,7,1,3,6,9]
+Output: [4,7,2,9,6,3,1]
+
+Input: root = [2,1,3]
+Output: [2,3,1]
+
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        //base case
+        // return without recursion
+        if (root != null){
+            invertTree(root.left);
+            invertTree(root.right);
+            
+            TreeNode tmp = root.right;
+            root.right = root.left;
+            root.left = tmp;
+        }
+        return root;
+    }
+}
